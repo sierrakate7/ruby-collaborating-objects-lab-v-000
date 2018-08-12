21d0baa038aa376d50be5f903c036bb7970a70d9
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-12 14:42:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
   
    def self.new_by_filename(file)
     full_title = file.split(" - ")
-    song = self.new 
+    song = self.new (full_title[1])
+    
   end
   
    def artist_name=(name)
